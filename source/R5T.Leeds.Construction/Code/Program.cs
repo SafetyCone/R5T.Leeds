371e8847c262c140c5b926cacabb55db752afe11
MinexAutomation <minex.automation@gmail.com>
2019-12-10 13:00:55 -0800

    Added test of configuration-based, machine location-aware custom secrets directory path provider.

--- source/R5T.Leeds.Construction/Code/Program.cs
+++ source/R5T.Leeds.Construction/Code/Program.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Suebia;
+
+
 namespace R5T.Leeds.Construction
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program.TestSecretsDirectoryPathProvider();
+        }
+
+        private static void TestSecretsDirectoryPathProvider()
+        {
+            var serviceProvider = Program.GetServiceProvider();
+
+            var secretsDirectoryPathProvider = serviceProvider.GetRequiredService<ISecretsDirectoryPathProvider>();
+
+            var secretsDirectoryPath = secretsDirectoryPathProvider.GetSecretsDirectoryPath();
+
+            Console.WriteLine($"Secrets directory path: {secretsDirectoryPath}");
+        }
+
+        private static IServiceProvider GetServiceProvider()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<IConfiguration>(configuration)
+                .UseMachineLocationAwareCustomSecretsDirectoryPath()
+                .BuildServiceProvider();
+
+            return serviceProvider;
         }
     }
 }
